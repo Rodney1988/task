ca9873ae1a06bdf7a53e0429164eb76f5456c42e
Rodrigo Wahl <rodvel8@hotmail.com>
2022-09-19 21:55:07 +0200

    upgrade readme and dependencies

--- src/helpers/Crawl.jsx
+++ src/helpers/Crawl.jsx
@@ -6,21 +6,24 @@ import { Typography } from "@mui/material";
 // Note: Because I could not get the library to work properly (only renders black screen), I opted for simply rendering the texts using the usual Typography components
 
 export const StarWarsCrawl = () => {
-  const [film, setFilm] = useState(null);
-  const { crawl } = useParams();
+    const [film, setFilm] = useState(null);
+    const { crawl } = useParams();
 
-  useEffect(() => {
-    fetch(`https://www.swapi.tech/api/films/${crawl}`)
-      .then((r) => r.json())
-      .then((data) => setFilm(data.result.properties));
-  }, [crawl]);
+    useEffect(
+        () => {
+            fetch(`https://www.swapi.tech/api/films/${crawl}`)
+                .then(r => r.json())
+                .then(data => setFilm(data.result.properties));
+        },
+        [crawl]
+    );
 
-  if (!film) return <Typography variant="h3">Loading...</Typography>;
-  // return <Crawl title={film.title} text={film.opening_crawl} />;
-  return (
-    <div>
-      <Typography variant="h3">{film.title}</Typography>
-      <Typography>{film.opening_crawl}</Typography>
-    </div>
-  );
+    if (!film) return <Typography variant="h3">Loading...</Typography>;
+    // return <Crawl title={film.title} text={film.opening_crawl} />;
+    return (
+        <div>
+            <Typography variant="h3">{film.title}</Typography>
+            <Typography>{film.opening_crawl}</Typography>
+        </div>
+    );
 };
